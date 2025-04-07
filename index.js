be4b4033d370ff1fb206349db8041d2a09fce323
Danscot <99121218+Danscot@users.noreply.github.com>
2025-04-07 15:35:26 +0100

    Update index.js
    I fixed an import issue on crypto module

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ import handleIncomingMessage from './events/messageHandler.js';
 
 import crypto from 'crypto';
 
-globalThis.crypto = crypto; // Ensure it's globally available
-
-
 (async () => {
 
     await connectToWhatsApp(handleIncomingMessage);
